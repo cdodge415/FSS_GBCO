10b8ab942dc9734e0cad5026627827fba4ca6f85
bolotinl <bolotinljb@gmail.com>
2020-08-03 10:20:08 -0700

    Updated queries for ALL disch and SC data in GBCO
    Not just sites with overlapping disch and SC data

--- Chloe_USGS_DataFilter.R
+++ Chloe_USGS_DataFilter.R
@@ -1,29 +1,57 @@
 setwd("C:/Users/cdodg/Desktop/Blaszczak lab/FSS project/Data from Phil")
-#data is for all of US? need to figure out which observations
-#are for GB and CO river basins (HUC 14, 15, 16)
-#call packages from library
-#library(ggplot2)
-#install.packages("ggpubr")
+# Data is for all of US
+# Need to filter for GB and CO River basins (HUC 14, 15, 16)
+library(ggplot2)
 library(ggpubr)
-#theme_set(theme_pubr())
+theme_set(theme_pubr())
 library(tidyverse)
 library(dplyr)
-#read data
-disch_data <- readRDS("disch_query.rds")
-SC_data <- readRDS("SC_query.rds")
-#bar plot of hucIDs
-#ggplot(disch_data, aes(huc_cd)) +
-#geom_bar(fill = "#0073C2FF") +
-#theme_pubclean()
-#confirmed data is for all of US
-#need to filter for HUC 14,15,16 with >=365 days of obs
-disch_huc_sites <- disch_data %>% filter(str_detect(huc_cd, "^14|^15|^16") & count_nu>=365) %>% select(Site_ID,station_nm,Lat,Lon,huc_cd,parm_cd,begin_date,end_date,count_nu,site_tp_cd,data_type_cd,access_cd)
-SC_huc_sites <- SC_data %>% filter(str_detect(huc_cd, "^14|^15|^16") & count_nu>=365) %>% select(Site_ID,station_nm,Lat,Lon,huc_cd,parm_cd,begin_date,end_date,count_nu,site_tp_cd,data_type_cd,access_cd)
+
+# Read in data
+disch_sensor <- read.csv("all_disch_lotic_sensor.csv")
+SC_sensor <- read.csv("all_SC_lotic_sensor.csv")
+# disch_SC_sensor <- ("disch_SC_sensor.csv")
+# Chloe: we don't really want to filter this, this still has sites of all types, not just lotic
+# disch_data <- readRDS("disch_query.rds")
+# SC_data <- readRDS("SC_query.rds")
+
+# # Bar plot of hucIDs
+# ggplot(disch_data, aes(huc_cd)) +
+# geom_bar(fill = "#0073C2FF") +
+# theme_pubclean()
+# # Confirmed data is for all of US
+
+# Need to filter for HUC 14,15,16 with >=365 days of obs, assuming this gives us at least 1 rough year of data
+# Lauren replaced disch_data and SC_data here with disch_sensor and SC_sensor
+disch_huc_sites <- disch_sensor %>% 
+  filter(str_detect(huc_cd, "^14|^15|^16") & count_nu>=365) %>% 
+  select(Site_ID,station_nm,Lat,Lon,huc_cd,parm_cd,begin_date,end_date,count_nu,site_tp_cd,data_type_cd,access_cd)
+  disch_huc_sites$Site_ID <- factor(disch_huc_sites$Site_ID)
+  levels(disch_huc_sites$Site_ID) # 2654
+SC_huc_sites <- SC_sensor %>% 
+  filter(str_detect(huc_cd, "^14|^15|^16") & count_nu>=365) %>% 
+  select(Site_ID,station_nm,Lat,Lon,huc_cd,parm_cd,begin_date,end_date,count_nu,site_tp_cd,data_type_cd,access_cd)
+  SC_huc_sites$Site_ID <- factor(SC_huc_sites$Site_ID)  
+  levels(SC_huc_sites$Site_ID) # 193
+# disch_SC_huc_sites <- disch_SC_sensor %>% 
+#   filter(str_detect(huc_cd, "^14|^15|^16") & count_nu>=365) %>% 
+#   select(Site_ID,station_nm,Lat,Lon,huc_cd,parm_cd,begin_date,end_date,count_nu,site_tp_cd,data_type_cd,access_cd)
+
+
 saveRDS(disch_huc_sites, "GBCO_disch_sites.rds")
 saveRDS(SC_huc_sites, "GBCO_SC_sites.rds")
 
-overlap_huc_sites <- SC_huc_sites[(SC_huc_sites$Site_ID%in% disch_huc_sites$Site_ID), ]
-saveRDS(overlap_huc_sites, "GBCO_dischSC_sites.rds")
+overlap_huc_sites <- intersect(disch_huc_sites$Site_ID, SC_huc_sites$Site_ID)
+overlap_huc_sites <- SC_huc_sites[which(SC_huc_sites$Site_ID %in% overlap_huc_sites),]
+class(overlap_huc_sites$Site_ID)
+overlap_huc_sites$Site_ID <- factor(overlap_huc_sites$Site_ID)
+levels(overlap_huc_sites$Site_ID)
+
+saveRDS(overlap_huc_sites, "GBCO_dischSC_sites.rds") # 182
+
+# overlap_huc_sites <- SC_huc_sites[(SC_huc_sites$Site_ID%in% disch_huc_sites$Site_ID), ]
+# saveRDS(overlap_huc_sites, "GBCO_dischSC_sites.rds")
+
 #saveRDS(disch_huc_sites, "disch_filtered_sites.rds")
 #saveRDS(SC_huc_sites, "SC_filtered_sites.rds")
 
--- Example queries.R
+++ Example queries.R
@@ -13,8 +13,6 @@ library("dplyr") #For filtering
 source(here("concurrent_overlap.R"))
 
 #Read in the data queries
-  # disch_query <- readRDS(here("output", "disch_query.rds")) 
-  # SC_query <- readRDS(here("output", "SC_query.rds"))
  disch_query <- readRDS(here("disch_query.rds"))
  SC_query <- readRDS(here("SC_query.rds"))
 
@@ -25,38 +23,66 @@ source(here("concurrent_overlap.R"))
     filter(site_tp_cd %in% c("ST", "ST-CA", "ST-DCH", "ST-TS", "SP")) %>%
     filter(data_type_cd == "dv" | data_type_cd == "uv") %>%
     filter(!is.na(Lat)) %>%
-    distinct(Site_ID, Lat, Lon)
-        
+    distinct(Site_ID, Lat, Lon) # Flowing waters with SC
+    
+    class(SC_lotic_sensor$Site_ID)
+    SC_lotic_sensor$Site_ID <- factor(SC_lotic_sensor$Site_ID)
+    levels(SC_lotic_sensor$Site_ID) # 2811
+ 
+  disch_lotic_sensor <- disch_query %>%
+   filter(site_tp_cd %in% c("ST", "ST-CA", "ST-DCH", "ST-TS", "SP")) %>%
+   filter(data_type_cd == "dv" | data_type_cd == "uv") %>%
+   filter(!is.na(Lat)) %>%
+   distinct(Site_ID, Lat, Lon) # Flowing waters with Q
+  
+   class(disch_lotic_sensor$Site_ID)
+   disch_lotic_sensor$Site_ID <- factor(disch_lotic_sensor$Site_ID)
+   levels(disch_lotic_sensor$Site_ID) # 23425
+  
+  
+  
+  # Write 2 csv's: 1 for all lotic sites with SC and 1 for all lotic sites with Q, this is all data we want
+  SC_lotic_sensor <- subset(SC_query, SC_query$Site_ID %in% SC_lotic_sensor$Site_ID)
+  disch_lotic_sensor <- subset(disch_query, disch_query$Site_ID %in% disch_lotic_sensor$Site_ID)
+  
+  write.csv(SC_lotic_sensor, "all_SC_lotic_sensor.csv") # as opposed to SC_only which is sites that have SC and not disch
+  write.csv(disch_lotic_sensor,"all_disch_lotic_sensor.csv") # as opposed to disch_only which is sites that have disch and not SC
+  
+  
+# Concurrent overlap doesn't necessarily matter for us since we are doing streamgage matching 
 #-------------------------------------------------
 #Filter for all lotic NWIS sites with specific conductivity and discharge sensors (subdaily or daily)  
 #-------------------------------------------------
-  #Since the specific conductivity query contains two different parameter codes ("00094", "00095")
-  #We have to do a little trick to get the overlap function to work by giving them all the same parameter code
-    SC_query2 <- SC_query
-    SC_query2$parm_cd <- "00000"
-
-  #Find concurrent overlap of data
-    disch_SC_sensor <- overlap_fun(
-      param_queries = list(disch_query, SC_query2), 
-      site_types = c("ST", "ST-CA", "ST-DCH", "ST-TS", "SP"), 
-      data_types = c("uv", "dv"), 
-      min_obs = 1
-    )
-  # there are 2002 sites in the COUNTRY with SC and Discharge sensors  
-    
-write.csv(disch_SC_sensor, "disch_SC_sensor.csv")
-
-#See what sites have SC that do not have discharge    
-SC_noQ <- setdiff(SC_lotic_sensor, disch_SC_sensor) # 811 sites
-unique(SC_query$Site_ID)
-SC_query[duplicated(SC_query$Site_ID),]
-SC_noQ <- SC_query[which(SC_query$Site_ID %in% SC_noQ$Site_ID),]
-SC_noQ$Site_ID <- as.factor(SC_noQ$Site_ID)  
-levels(SC_noQ$Site_ID) 
-SC_noQ$site_tp_cd <- as.factor(SC_noQ$site_tp_cd)
-levels(SC_noQ$site_tp_cd) # already filtered for only flowing waters
-
-write.csv(SC_noQ, "SC_sensor_only.csv")
+#   #Since the specific conductivity query contains two different parameter codes ("00094", "00095")
+#   #We have to do a little trick to get the overlap function to work by giving them all the same parameter code
+#     SC_query2 <- SC_query
+#     SC_query2$parm_cd <- "00000"
+# 
+#   #Find concurrent overlap of data
+#     disch_SC_sensor <- overlap_fun(
+#       param_queries = list(disch_query, SC_query2), 
+#       site_types = c("ST", "ST-CA", "ST-DCH", "ST-TS", "SP"), 
+#       data_types = c("uv", "dv"), 
+#       min_obs = 1
+#     )  # there are 2002 sites with flowing water in the COUNTRY with SC and Discharge sensors  
+#     
+# write.csv(disch_SC_sensor, "disch_SC_sensor.csv") # Flowing waters with Q and SC
 
 
-#disch_query$HUC <- substr(disch_query$huc_cd,1,2)
+# #See what sites have SC that do not have discharge    
+# SC_only <- setdiff(SC_lotic_sensor, disch_SC_sensor) # 811 sites
+# unique(SC_query$Site_ID)
+# SC_query[duplicated(SC_query$Site_ID),]
+# SC_only <- SC_query[which(SC_query$Site_ID %in% SC_only$Site_ID),]
+# # SC_only$Site_ID <- as.factor(SC_only$Site_ID)  
+# # levels(SC_only$Site_ID) 
+# # SC_only$site_tp_cd <- as.factor(SC_only$site_tp_cd)
+# # levels(SC_only$site_tp_cd) # already filtered for only flowing waters
+# 
+# write.csv(SC_only, "SC_sensor_only.csv")
+# 
+# #See what sites have discharge that do not have SC
+# disch_only <- setdiff(disch_lotic_sensor, disch_SC_sensor)
+# disch_only <- disch_query[which(disch_query$Site_ID %in% disch_only$Site_ID),]
+# 
+# write.csv(disch_only, "disch_sensor_only.csv")
