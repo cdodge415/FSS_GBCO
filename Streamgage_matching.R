ddd2b582de10fa296691a96fc7bcdfcf23a5aa13
bolotinl <bolotinljb@gmail.com>
2020-10-01 10:32:00 -0700

    Update Streamgage_matching.R

--- Streamgage_matching.R
+++ Streamgage_matching.R
@@ -14,3 +14,20 @@ write.csv(info, "Streamgage_Matches_subset.csv")
 
 # once sites are FINALIZED we can see which of those sites are already matched here and can either 
 # have their matches used, or need new matches
+
+library(sf)
+flowlinesn <-  st_read("/Volumes/Blaszczak Lab/FSS/NHD/NHD_H_1606_HU4_Shape/NHDFlowline.shp")
+
+
+> # Bring in list of sites with continuous SC data
+  > setwd("/Users/laurenbolotin/Desktop/Blaszczak Lab/GB CO WQ Data/WQP Formatted Meta")
+> continuous <- read.csv("WQ_USGS_SCcontinuous_site_list.csv")
+> View(continuous)
+> class(continuous$SiteID)
+[1] "character"
+> continuous$SiteID <- as.factor(continuous$SiteID)
+> sites <- levels(continuous$SiteID)
+> sites <- as.data.frame(sites)
+> View(sites)
+> setwd("/Volumes/Blaszczak Lab/FSS/All Data")
+> saveRDS(sites, "continuous_sites_usgs.rds")
